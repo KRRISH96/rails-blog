1b7e24f0228a69b662131ebaca3578b459208650
sai <kskprasad96@gmail.com>
2019-05-06 20:03:26 +0530

    Add edit action and link between edit, new, show, index views

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -14,12 +14,28 @@ class ArticlesController < ApplicationController
       flash[:notice] = "Article was successfully saved!"
       redirect_to article_path(@article)
     else
+      flash[:error] = "Article was not saved!"
       render 'new'
     end
   end
 
   def show
     @article = Article.find(params[:id])
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Article was updated successfully!"
+      redirect_to article_path(@article)
+    else
+      flash[:error] = "Article not updated!"
+      render 'edit'
+    end
   end
 
   private
