b273de239075dc1047493e4f15ec636ec522d265
sai <kskprasad96@gmail.com>
2019-05-08 19:41:26 +0530

    Add show page for user articles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:edit, :update]
+  before_action :set_user, only: [:edit, :update, :show]
 
   def new
     @user = User.new
@@ -25,6 +25,9 @@ class UsersController < ApplicationController
     else
       render 'edit'
     end
+  end
+
+  def show
   end
 
   private
