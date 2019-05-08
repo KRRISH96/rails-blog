97a5e7b2169c0417f17d5c2a4b3c7ba976b103c5
sai <kskprasad96@gmail.com>
2019-05-08 19:51:54 +0530

    Add users show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,10 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def index
+    @all_users = User.all
+  end
+
   def create
     @user = User.new(user_params)
     if @user.save
